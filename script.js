0f844e43392c2ac24903fe29bbb815ad8cca2513
emijay <hilmi.johari@yahoo.com.sg>
2019-06-26 17:25:15 +0800

    Uploading lucky-draw-game to Github

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
-console.log("hello script js");
+
+
+let timesPlayed = 0;
+
+let timesWon = 0;
+
+let winRatio = 0;
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+
+    let secretNumber = randomNum();
+
+    let guessedNumber = parseInt(currentInput);
+
+    console.log('Secret number is ' + secretNumber);
+
+    console.log('Chosen number is ' + guessedNumber);
+
+    hasWon(guessedNumber, secretNumber);
+
+    timesPlayed ++;
+
+    winRatio = (timesWon/timesPlayed * 100).toFixed(2);
+
+    document.getElementById("timesPlayed").textContent = timesPlayed;
+
+    document.getElementById("timesWon").textContent = timesWon;
+
+    document.getElementById("winRatio").textContent = winRatio;
+
 };
+
+// function signature
+// guessedNumber(number), secretNumber(number) --> won(boolean)
+
+// function header:
+// hasWon( guessedNumber, secretNumber )
+
+
+const hasWon = function(guessedNumber, secretNumber) {
+    if (guessedNumber === secretNumber) {
+        display('You have won!');
+
+        timesWon ++;
+
+    } else {
+        display('Please try again!')
+    }
+}
+
+const randomNum = function() {
+    return Math.floor(Math.random() * 10);
+}
